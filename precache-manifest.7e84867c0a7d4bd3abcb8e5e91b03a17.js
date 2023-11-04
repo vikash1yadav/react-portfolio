4fe3dbf87130a85f8280dad378637723153f49a9
vikash1yadav <64723750+vikash1yadav@users.noreply.github.com>
2023-11-04 04:22:33 +0000

    Updates

---
+++ precache-manifest.7e84867c0a7d4bd3abcb8e5e91b03a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31322c2602813459ca095cafef9f90ce",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "779d1b9e375a0ec59b81",
+    "url": "/react-portfolio/static/css/main.5f9b9700.chunk.css"
+  },
+  {
+    "revision": "50db40081e788e44958e",
+    "url": "/react-portfolio/static/js/2.393a8ca2.chunk.js"
+  },
+  {
+    "revision": "0b8eb0972663db1c547080c0f7afe375",
+    "url": "/react-portfolio/static/js/2.393a8ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779d1b9e375a0ec59b81",
+    "url": "/react-portfolio/static/js/main.ff7f7afd.chunk.js"
+  },
+  {
+    "revision": "624bfe83ca615183ba8a",
+    "url": "/react-portfolio/static/js/runtime-main.4a88bb1c.js"
+  },
+  {
+    "revision": "30b9c2d829a7ccbf7d4cc237fc36a6b1",
+    "url": "/react-portfolio/static/media/vikas.30b9c2d8.webp"
+  }
+]);
